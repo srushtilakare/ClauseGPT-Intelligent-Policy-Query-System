37ca1dfe054231852c08d8f8b5948abf24e4b71b
Saniya Pathan <saniyapathan1606@gmail.com>
2025-07-19 23:34:50 +0530

    backend change

--- backend/parser/docx_parser.py
+++
@@ -1,6 +0,0 @@
-from docx import Document
-
-def extract_text_from_docx(filepath):
-    doc = Document(filepath)
-    text = "\n".join([para.text for para in doc.paragraphs if para.text.strip()])
-    return text
--- backend/parser/pdf_parser.py
+++
@@ -1,6 +0,0 @@
-import fitz  # PyMuPDF
-
-def extract_text_from_pdf(filepath):
-    doc = fitz.open(filepath)
-    text = "\n".join([page.get_text() for page in doc])
-    return text
